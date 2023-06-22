683b2ea39351db9aab353fde20cc0488a410f02f
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-06-21 22:03:01 -0700

    upload home page skeleton code

--- AngularFrontend/src/app/home/home.component.ts
+++ AngularFrontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(
+    private _route: ActivatedRoute
+  ) {
+  }
 
+  ngOnInit(): void {
+  }
 }
